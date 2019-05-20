a0e3c9c53fa1d851c8438117511a6e6119e839bc
railway <doittherailway@gmail.com>
2019-05-19 18:55:49 -0700

    Prepare for heroku deployment

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    entry: [
+        './src/index.js'
+    ],
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: 'main.js'
+    }
+};
